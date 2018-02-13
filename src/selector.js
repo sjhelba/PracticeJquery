0544c81c242794d53bd85eb20a9f30d3b5706aba
Sara Jo Helba <29603156+sjhelba@users.noreply.github.com>
2018-02-13 10:57:55 -0500

    work

--- src/selector.js
+++ src/selector.js
@@ -18,7 +18,19 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 // return one of these types: id, class, tag.class, tag
 
 var selectorTypeMatcher = function(selector) {
-  // your code here
+  const things = ['.', '#']
+  const types = ['class', 'id']
+  let type;
+  if (things.indexOf(selector[0]) < 0) {
+    if (selector.indexOf('.') > -1) {
+      type = 'tag.class';
+    } else {
+      type = 'tag';
+    }
+  } else {
+    type = types[things.indexOf(selector[0])];
+  }
+  return type;
 };
 
 
@@ -32,16 +44,25 @@ var matchFunctionMaker = function(selector) {
   var matchFunction;
   if (selectorType === "id") {
     // define matchFunction for id
-
+    matchFunction = function (node) {
+      return node.id === selector.slice(1);
+    };
   } else if (selectorType === "class") {
     // define matchFunction for class
-
+    matchFunction = function (node) {
+      return node.classList.contains(selector.slice(1));
+    };
   } else if (selectorType === "tag.class") {
     // define matchFunction for tag.class
-
+    matchFunction = function (node) {
+      const [tag, className] = selector.split('.');
+      return node.classList.contains(className) && node.tagName.toLowerCase() === tag;
+    };
   } else if (selectorType === "tag") {
     // define matchFunction for tag
-
+    matchFunction = function (node) {
+      return node.tagName.toLowerCase() === selector;
+    };
   }
   return matchFunction;
 };
